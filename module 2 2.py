97d0b59f400ff335804bd787033b01402c7aab6b
Maytreya777 <maytreya777@gmail.com>
2024-07-10 00:55:09 +0300

    Add files via upload

---
+++ module 2 2.py
@@ -0,0 +1,11 @@
+first = (121)
+second = (26)
+third = (94)
+
+if first == second and second == third and third == first:
+    print(3)
+elif first == second or second == third or third == first:
+    print(2)
+else:
+    print(0)
+
